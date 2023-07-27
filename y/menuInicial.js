807b9b231a6759078e50077e2ebaea3ae2f6e731
Catarino Grosser Ferreira <87507933+catarino-grosser@users.noreply.github.com>
2023-07-27 14:40:51 -0300

    Add files via upload

---
+++ y/menuInicial.js
@@ -0,0 +1,8 @@
+function openMenu(){
+    document.getElementById("menuInicial").style.width="60%";
+    
+   } 
+function closeMenu() {
+    document.getElementById("menuInicial").style.width="0";
+    
+}
